c0fe53e2134c079563dcf14fca56129386e81b78
sato-ryosuke-2021 <sato.ryosuke@jvckenwood.com>
2021-07-15 10:48:53 +0900

    7include branch Commit

--- FizzBuzz.kt
+++ FizzBuzz.kt
@@ -1,3 +1,5 @@
+val list = listOf<Int>(7, 17, 27, 37, 47, 57, 67, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 87, 97)
+
 fun main() 
 {
     val range: IntRange = 1..100
@@ -12,8 +14,10 @@ fun printFizzBuzz (max: Int)
     	print("${convertFizzBuzz(cnt)} ")
 }
 
-fun convertFizzBuzz (printNumber: Int): String =
-    if (printNumber%15 == 0) {
+fun convertFizzBuzz (printNumber: Int): String = 
+    if (printNumber in list) {
+        "GitHub"
+    } else if (printNumber%15 == 0) {
         "FizzBuzz"
     } else if (printNumber%3 == 0) {
         "Fizz"
